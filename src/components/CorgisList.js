21b016febfc536db76bf953de0aac58c565fcbc1
soheecho94 <soheecho94@gmail.com>
2017-12-20 23:13:44 +0900

    add key for img

--- src/components/CorgisList.js
+++ src/components/CorgisList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const CorgisList = ({ corgis }) => {
-  const renderCorgis = corgis.map(corgi =>
-    <img src={corgi.images.fixed_height.url} />
+  const renderCorgis = corgis.map((corgi, index) =>
+    <img key={index} src={corgi.images.fixed_height.url} />
   )
 
   return (
