7730a233fb758f8c314f0539f953401097f065b7
soheecho94 <soheecho94@gmail.com>
2017-12-20 19:07:52 +0900

    add basic layout & css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>CORGIS GALORE</h1>
+        <div className="navbar">
+          <p>Navbar Component</p>
+        </div>
+        <div className="main-content">
+          <h1>CORGIS GALORE</h1>
+          <p>Corgis Component</p>
+        </div>
+        <div className="footer">
+          Footer
+        </div>
       </div>
     );
   }
