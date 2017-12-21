f40eb962fed28c9f431c1599492acfaf470018f9
soheecho94 <soheecho94@gmail.com>
2017-12-21 17:53:24 +0900

    import redux routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { fetchCorgis } from './actions/corgisAction'
 import CorgisList from './components/CorgisList';
 import './App.css';
