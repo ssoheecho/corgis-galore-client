b3227033aba77f1f698611000cd7665abfbb6a48
soheecho94 <soheecho94@gmail.com>
2017-12-22 12:44:09 +0900

    add corgisshow component, hyperlink images

--- src/components/CorgisList.js
+++ src/components/CorgisList.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CorgisList = ({ corgis }) => {
   const renderCorgis = corgis.map((corgi, index) =>
-    <img key={index} src={corgi.images.fixed_height.url} alt={corgi.title} />
+    <Link key={corgi.id} to={`/corgis/${corgi.id}`}><img key={index} src={corgi.images.fixed_height.url} alt={corgi.title} /></Link>
   )
 
   return (
---
+++ src/components/CorgisShow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CorgisShow = () => {
+
+  return (
+    <div className="corgis-show">
+      <h3>Corgis Show Page!</h3>
+    </div>
+  )
+}
+
+export default CorgisShow
