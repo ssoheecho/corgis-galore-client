12d54967e67acafeaf06df1aecb17f8242726860
soheecho94 <soheecho94@gmail.com>
2017-12-25 00:40:33 +0900

    edit CorgisShow component to be container, add fetchcomments

--- src/components/corgis/CorgisShow.js
+++ src/components/corgis/CorgisShow.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchComments } from '../../actions/commentsAction'
 
-const CorgisShow = ({ corgi }) =>
-  <div className="corgis-show">
-    <h3>breadloaf</h3>
-    <div className="corgi-gif">
-      <img src={corgi.images.fixed_height.url} />
-    </div>
-    <div className="comments">
-    </div>
-  </div>
+class CorgisShow extends Component {
+  componentDidMount() {
+    this.props.fetchComments();
+  }
+
+  render() {
+    return (
+      <div className="corgis-show">
+        <h3>breadloaf</h3>
+        <div className="corgi-gif">
+          <img src={this.props.corgi.images.fixed_height.url} />
+        </div>
+        <div className="comments">
+        </div>
+      </div>
+    )
+  }
+}
 
 function mapStateToProps(state, ownProps) {
   const corgi = state.corgis.find(corgi => corgi.id === ownProps.match.params.corgiId)
@@ -21,4 +32,8 @@ function mapStateToProps(state, ownProps) {
   }
 }
 
-export default connect(mapStateToProps)(CorgisShow)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchComments }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CorgisShow)
