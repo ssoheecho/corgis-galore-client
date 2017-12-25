4711ca67035a85f034a0c0dd8442fa4c029f5d7e
soheecho94 <soheecho94@gmail.com>
2017-12-25 19:18:22 +0900

    add body component

---
+++ src/components/Body.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Body = () => {
+  return(
+    <div className="main-content-body">
+      <p>Do you ❤️ corgis? Well, you've come to the right place!<br />
+      Get your daily dose of corgis here by clicking below ⬇️</p>
+      <a id="breadloaf" href="/corgis">🍞</a>
+      <p>Sourced from Giphy <br />
+      Made with lots of ❤️🧡💛💚💙💜
+      </p>
+    </div>
+  )
+}
+
+export default Body
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Body from './Body'
 
 const Home = () => {
   return(
@@ -6,14 +7,7 @@ const Home = () => {
       <div className="main-content-title">
         <h1>CORGIS GALORE</h1>
       </div>
-      <div className="main-content-body">
-        <p>Do you ❤️ corgis? Well, you've come to the right place!<br />
-        Get your daily dose of corgis here by clicking below ⬇️</p>
-        <a id="breadloaf" href="/corgis">🍞</a>
-        <p>Sourced from Giphy <br />
-        Made with lots of ❤️🧡💛💚💙💜
-        </p>
-      </div>
+      <Body />
     </div>
   )
 }
