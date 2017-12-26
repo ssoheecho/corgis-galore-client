75dc0a24c7c315629288df9e6ae995eeaf4ecea0
soheecho94 <soheecho94@gmail.com>
2017-12-26 23:42:14 +0900

    add contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+  return(
+    <div className="contact">
+      <p>© Questions about the page? Please 💌 soheecho94@gmail.com</p>
+    </div>
+  )
+}
+
+export default Contact
