2d40ad13264f1c9f7ae22e1e0d9c757adc5c5b4a
soheecho94 <soheecho94@gmail.com>
2017-12-28 19:35:44 +0900

    link each gif to show page

--- src/components/corgis/FavoriteCorgis.js
+++ src/components/corgis/FavoriteCorgis.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 class FavoriteCorgis extends Component {
   render() {
     const favoriteCorgis = this.props.corgis.filter(corgi => corgi.favorite === true)
-    const favoriteCorgi = favoriteCorgis.map(corgi => <img alt={corgi.title} key={corgi.id} src={corgi.images.fixed_height.url} />)
+    const favoriteCorgi = favoriteCorgis.map(corgi => <Link key={corgi.id} to={`/corgis/${corgi.id}`}><img alt={corgi.title} key={corgi.id} src={corgi.images.fixed_height.url} /></Link>)
 
     return (
       <div className="favorite-corgis">
-        <h2>Favorite Corgis:</h2>
+        <h3>Favorite Corgis</h3>
         {favoriteCorgi}
       </div>
     )
