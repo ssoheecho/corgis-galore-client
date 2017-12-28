ff4cc6cacc9aa1dff78e29fe8d36a499dfd0f28b
soheecho94 <soheecho94@gmail.com>
2017-12-28 18:07:59 +0900

    filter favorites

--- src/components/corgis/FavoriteCorgis.js
+++ src/components/corgis/FavoriteCorgis.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class FavoriteCorgis extends Component {
   render() {
+    const favoriteCorgis = this.props.corgis.filter(corgi => corgi.favorite === true)
+    const favoriteCorgi = favoriteCorgis.map(corgi => <img src={corgi.images.fixed_height.url} />)
+
     return (
       <div className="favorite-corgis">
         <h2>Favorite Corgis Below:</h2>
+        {favoriteCorgi}
       </div>
     )
   }
 }
 
-export default FavoriteCorgis
+export default connect(state=> {
+  return {
+    corgis: state.corgis
+  }
+})(FavoriteCorgis)
