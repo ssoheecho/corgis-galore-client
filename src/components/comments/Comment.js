3dca5cdfd6d22ddde160dc338ec8d64b609ba1d5
soheecho94 <soheecho94@gmail.com>
2017-12-28 19:30:32 +0900

    change positions for add/delete buttons, add thumbs up emoji to label

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { removeComment } from '../../actions/commentsAction'
+import { updateLikes } from '../../actions/commentsAction'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 class Comment extends Component {
+
+  handleOnClickLike = () => {
+    const updateInfo = {
+      likes: this.props.comment.likes += 1,
+      id: this.props.comment.id
+    }
+    this.props.updateLikes(updateInfo);
+  }
 
   handleOnClick = () => {
     this.props.removeComment(this.props.comment)
@@ -16,6 +25,8 @@ class Comment extends Component {
         <li key={this.props.comment.id}>
           <span className="comment">{this.props.comment.content}</span>
         </li>
+        <label className ="likes">{this.props.comment.likes}👍🏻</label>
+        <button className="like-button" onClick={this.handleOnClickLike}>like</button>
         <button className="delete-button" onClick={this.handleOnClick}>X</button>
       </div>
     )
@@ -23,9 +34,7 @@ class Comment extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    removeComment
-  }, dispatch)
+  return bindActionCreators({ removeComment, updateLikes }, dispatch)
 }
 
 export default connect(null, mapDispatchToProps)(Comment);
